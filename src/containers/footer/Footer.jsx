111dec2686f4e40f039b5dca738fa5a78c3cd8b4
moaquib10 <moaquib10@gmail.com>
2023-11-25 00:00:14 +0530

    first commit

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer-wrapper">
       <div className="footer-heading">
-        <h1 className="footer-title">Do you want to step into the future before others?</h1>
+        <h1 className="footer-title">
+          Do you want to step into the future before others?
+        </h1>
       </div>
       <div className="footer-button">
         <p className="button-text">Request Early Access</p>
       </div>
       <div className="footer-links">
-        <div className="footer-links-logo">
-          <h2>Convosphere.ai</h2>
-          <p className="logo-text">&copy; 2023 Convosphere.ai. All rights reserved.</p>
+        <div className="footer-links-div">
+          <h2 className="logo-text">Convosphere.ai</h2>
+          <p>&copy; 2023 Convosphere.ai. All rights reserved.</p>
         </div>
         <div className="footer-links-div">
-          <h4 className="company-title">Company</h4>
-          <p className="footer-link">Terms & Conditions</p>
-          <p className="footer-link">Privacy Policy</p>
-          <p className="footer-link">Contact</p>
+          <h4>COMPANY</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
         </div>
         <div className="footer-links-div">
-          <h4 className="get-in-touch-title">Get in touch</h4>
-          <p className="contact-info">info@payme.net</p>
+          <h4>GET IN TOUCH</h4>
+          <p>convosphere2023@gmail.com</p>
         </div>
       </div>
     </div>
